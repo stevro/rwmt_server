6a952658ae839ff6ee3ac49835e3a7f357b8f62a
Stefan Matei <stev.matei@gmail.com>
2014-08-03 16:00:19 +0300

    Moved the tenant auth to a listener on kernel.request

---
+++ src/Rwmt/Bundle/RwmtBundle/EventListener/TenantListener.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TenantListener
+ *
+ * @author stefan
+ */
+
+namespace Rwmt\Bundle\RwmtBundle\EventListener;
+
+class TenantListener
+{
+
+    private $em;
+
+    public function __construct(\Doctrine\ORM\EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function onKernelRequest(\Symfony\Component\HttpKernel\Event\GetResponseEvent $event)
+    {
+        if('cli' === php_sapi_name()){
+            return;
+        }
+
+        $conf = $this->em->getConfiguration();
+        $conf->addFilter(
+            'multi_tenant',
+            'Rwmt\Bundle\RwmtBundle\DoctrineFilters\MultiTenantFilter'
+        );
+
+        $filter = $this->em->getFilters()->enable('multi_tenant');
+
+        $request = $event->getRequest();
+
+        $apiKey = $request->headers->get('rwmt-auth');
+
+        if(null === $apiKey){
+            throw new \Symfony\Component\HttpKernel\Exception\HttpException(401, 'You must provide an API KEY!',null, array('Rwmt-Auth-Status' => 'Required'));
+        }
+
+        $tenant = $this->em->getRepository('RwmtBundle:Tenant')->findOneBy(array('apiKey' => $apiKey));
+
+        if(!$tenant){
+            throw new \Symfony\Component\HttpKernel\Exception\HttpException(401, 'Wrong API KEY!', null, array('Rwmt-Auth-Status' => 'Invalid'));
+        }
+
+        $filter->setParameter('tenantId', $tenant->getId(), 'integer');
+
+    }
+
+}
--- src/Rwmt/Bundle/RwmtBundle/RwmtBundle.php
+++ src/Rwmt/Bundle/RwmtBundle/RwmtBundle.php
@@ -7,28 +7,10 @@ use Rwmt\Bundle\RwmtBundle\Entity\Tenant;
 
 class RwmtBundle extends Bundle
 {
+
     public function boot()
     {
-        /* @var $em \Doctrine\ORM\EntityManager */
-        $em = $this->container->get('doctrine.orm.default_entity_manager');
-        $conf = $em->getConfiguration();
-        $conf->addFilter(
-            'multi_tenant',
-            'Rwmt\Bundle\RwmtBundle\DoctrineFilters\MultiTenantFilter'
-        );
 
-        $filter = $em->getFilters()->enable('multi_tenant');
-
-        if(!isset($_SERVER['HTTP_RWMT_AUTH'])){
-            return;
-        }
-
-        $tenant = $em->getRepository('RwmtBundle:Tenant')->findOneBy(array('apiKey' => $_SERVER['HTTP_RWMT_AUTH']));
-
-        if(!$tenant){
-            throw new \Symfony\Component\HttpKernel\Exception\HttpException(401, 'Wrong API KEY!', null, array('Rwmt-Auth-Status' => 'Invalid'));
-        }
-
-        $filter->setParameter('tenantId', $tenant->getId(), 'integer');
     }
+
 }
